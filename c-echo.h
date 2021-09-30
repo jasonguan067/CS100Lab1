6b9ad44f35333e2ced70dd55998719471acf789a
jasonguan067 <jasonguan067@gmail.com>
2021-09-30 14:37:07 -0700

    removed new line from echo, added to main2.

--- c-echo.h
+++ c-echo.h
@@ -2,12 +2,13 @@
 
 std::string echo(int length, char** chars) {
     std::string ret = "";
+  
     for(int i = 1; i < length; i++) {
         ret += chars[i];
         if(i < length - 1) {
             ret += " ";
         }
     }
-    ret += "\n";
+   
     return ret;
-}
+}	
